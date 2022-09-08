505a00ca288270a1d7edef0b0431ac71e3b9b55e
192110587 <112752216+192110587@users.noreply.github.com>
2022-09-08 12:51:31 +0530

    Add files via upload

---
+++ square and cube of numbers.cpp
@@ -0,0 +1,17 @@
+//K.Ashwini Reg.no- 192110587
+//Square and cube of numbers
+#include<iostream>
+using namespace std;
+//program on square and cube of a number
+int main()
+{
+  //Function Type is float
+  float num,cu,sq;
+  cout<<"Enter Any Number To Find square :\t";
+  cin>>num;
+  cu = num*num*num;   
+    sq= num*num;                               
+  cout<<"square Of "<<num<<" is = "<<sq<<endl<<endl;
+    cout<<"cube Of "<<num<<" is = "<<cu<<endl<<endl;
+  
+ }
