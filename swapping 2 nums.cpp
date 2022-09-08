c4c27442aa724eb3908efcee565a367292d55c1f
192110587 <112752216+192110587@users.noreply.github.com>
2022-09-08 13:25:25 +0530

    Add files via upload

---
+++ swapping 2 nums.cpp
@@ -0,0 +1,16 @@
+//K.Ashwini Reg.no- 192110587
+//Swapping two numbers
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 5, b = 10, temp;
+    cout << "Before swapping." << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+    temp = a;
+    a = b;
+    b = temp;
+    cout << "\nAfter swapping." << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+    return 0;
+}
